ef7eb26134e1be39902f81a9db437cffadf93d6c
PBush705 <PBush705@evit.edu>
2024-08-16 13:21:56 -0700

    exported from Skill Struck

---
+++ Python 2/parameters2.py
@@ -0,0 +1,28 @@
+def days(month):
+    if month == 1:
+        return print(31)
+    elif month == 2:
+        return print(28)
+    elif month == 3:
+        return print(31)
+    elif month == 4:
+        return print(30)
+    elif month == 5:
+        return print(31)
+    elif month == 6:
+        return print(30)
+    elif month == 7:
+        return print(31)
+    elif month == 8:
+        return print(31)
+    elif month == 9:
+        return print(30)
+    elif month == 10:
+        return print(31)
+    elif month == 11:
+        return print(30)
+    elif month == 12:
+        return print(31)
+cool = int(input())
+
+days(cool)
