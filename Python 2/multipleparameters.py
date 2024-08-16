5a40f2d436afaa9ef4fccbe3f973c6c7f8bdaf22
PBush705 <PBush705@evit.edu>
2024-08-16 13:21:56 -0700

    exported from Skill Struck

---
+++ Python 2/multipleparameters.py
@@ -0,0 +1,8 @@
+def fruits(apple, orange, banana):
+    print(apple + orange + banana)
+
+food1 = input("What's your first favorite food?")
+food2 = input("What's your second favorite food?")
+food3 = input("What's your third favorite food?")
+
+fruits(food1, food2, food3)
