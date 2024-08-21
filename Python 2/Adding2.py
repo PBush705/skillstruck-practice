79f32a83a75d64a617c025d9870c3c4251222f80
PBush705 <PBush705@evit.edu>
2024-08-21 12:18:37 -0700

    exported from Skill Struck

---
+++ Python 2/Adding2.py
@@ -0,0 +1,7 @@
+trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+
+remove = input("What tree do you wanna remove?")
+
+trees.pop(remove)
+
+print(trees)
