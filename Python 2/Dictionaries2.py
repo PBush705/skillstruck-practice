34f16fa33454910525f5a1203524c0bd0c6c9584
PBush705 <PBush705@evit.edu>
2024-08-21 12:18:35 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries2.py
@@ -0,0 +1,6 @@
+mountains = {"Timpanogos" : 2, "Everest" : 4, "Kilimanjaro" : 3, "Vesuvius" : 1, "Francs Peak" : 5}
+
+gotit = ["Oof", "Two", "Three", "Four", "Five"]
+
+print(mountains)
+print(gotit)
