3218c38569cd336615d0ad06ca5a18270b9e7f74
PBush705 <PBush705@evit.edu>
2024-08-21 12:18:34 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries.py
@@ -0,0 +1,7 @@
+mountains = {"Timpanogos" : 2, "Everest" : 4, "Kilimanjaro" : 3, "Vesuvius" : 1, "Francs Peak" : 5, "K2" : 6, "Kangchenjunga" : 7, "Lhotse" : 8, "Cho Oyu" : 9, "Dhaulagiri" : 10}
+
+print(mountains)
+
+print(mountains["Vesuvius"])
+
+mountains["Kilimanjaro"] = 4
