46b1417ba8475dda339b9df68925d3789d7d2ba9
PBush705 <PBush705@evit.edu>
2024-09-03 11:52:15 -0700

    exported from Skill Struck

---
+++ Python 2/LoopingContinued2.py
@@ -0,0 +1,13 @@
+words_count = {}
+words = input("Create a list of words").split()
+for word in words:
+	if word not in words_count:
+		words_count[word] = 0
+	words_count[word] += 1
+
+biggest = 0
+for word in word_count:
+	if word.values > 
+
+for:
+	if :
