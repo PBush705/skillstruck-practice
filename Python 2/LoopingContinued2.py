d401636b8aa8998ca9dd69c39fdd3568353d50d4
PBush705 <PBush705@evit.edu>
2024-09-13 12:59:56 -0700

    exported from Skill Struck

--- Python 2/LoopingContinued2.py
+++ Python 2/LoopingContinued2.py
@@ -1,13 +1,12 @@
 words_count = {}
 words = input("Create a list of words").split()
+biggest = ""
 for word in words:
-	if word not in words_count:
-		words_count[word] = 0
-	words_count[word] += 1
-
+    if word not in words_count:
+        words_count[word] = 0
+    words_count[word] += 1
 biggest = 0
-for word in word_count:
-	if word.values > 
-
-for:
-	if :
+for word in words_count:
+    biggest = max(words_count.values())
+    if words_count[word] == biggest:
+        print(word)
