fe4784eaf56f4a36c90021591b3d17c4d148dc63
PBush705 <PBush705@evit.edu>
2024-09-13 13:00:08 -0700

    exported from Skill Struck

---
+++ Python3/Multi-dimensional Lists.py
@@ -0,0 +1,9 @@
+first = ["Caleb", "Jordan", "Michael", "Bron"]
+last = ["Steveonson", "Bush", "Axel", "Boston"]
+cool = []
+
+for x in first:
+    col = []
+    for j in last:
+        cool.append(x + " " + j)
+    print(cool)
