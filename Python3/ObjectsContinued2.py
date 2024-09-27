d5f4184985826aff9b55d20c8680136e6c831cfe
PBush705 <PBush705@evit.edu>
2024-09-27 12:05:09 -0700

    exported from Skill Struck

---
+++ Python3/ObjectsContinued2.py
@@ -0,0 +1,20 @@
+class Friday:
+	def __init__(self, activity, friend):
+		self.activity = activity
+		self.friend = friend
+
+def pictures(self):
+    print("We took so many pictures!")
+thisWeekend = Friday("Movie", "Charlotte")
+
+thisWeekend.money = 20
+
+thisWeekend.rating = 10
+
+thisWeekend.friend = "Shane"
+
+print(thisWeekend)
+
+print(thisWeekend.money)
+
+print(thisWeekend.friend)
