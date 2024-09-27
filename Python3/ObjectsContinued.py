9ef4a688285a77d2f2ac74a8cd3fa6035e9639da
PBush705 <PBush705@evit.edu>
2024-09-27 12:05:08 -0700

    exported from Skill Struck

---
+++ Python3/ObjectsContinued.py
@@ -0,0 +1,17 @@
+class Shopping:
+	def __init__(self, item, quality):
+		self.item = item
+		self.quality = quality
+self.total = []
+def spending(self, cost):
+    self.total.append(cost)
+
+
+
+sportStore = Shopping("Kayak", "High Quality")
+
+sportStore.spending(3)
+sportStore.spending(6)
+sportStore.spending(9)
+
+print(sportStore.total)
