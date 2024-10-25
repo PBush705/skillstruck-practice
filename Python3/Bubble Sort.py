c37c628b3e53d743f560605c7997182c2d92f53b
PBush705 <PBush705@evit.edu>
2024-10-25 13:00:21 -0700

    exported from Skill Struck

---
+++ Python3/Bubble Sort.py
@@ -0,0 +1,19 @@
+arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+
+num = int(input("Give a number"))
+
+arr.append(num)
+
+
+def bubbleSort(sort_list):
+    n = len(sort_list)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if sort_list[j] > sort_list[j+1]:
+                temp = sort_list[j]
+                sort_list[j] = sort_list[j+1]
+                sort_list[j+1] = temp
+    return(sort_list)
+
+
+print(bubbleSort(arr))
